b2576e15fed437dda911d2e8170d7e5e7cc9438c
avebest1992 <77918703+avebest1992@users.noreply.github.com>
2021-03-01 17:59:35 +0800

    更改按钮图标显示
    并且格式化了代码

--- PipeDrawingDemo.py
+++ PipeDrawingDemo.py
@@ -4,7 +4,7 @@ import tkinter.filedialog
 from PIL import Image, ImageTk
 import os
 
-# 初始化屏幕、画布、画笔
+"""初始化屏幕、画布、画笔"""
 root = Tk()
 root.title('单线图绘制-By 周啸天')
 root.geometry('950x700+0+0')
@@ -19,7 +19,7 @@ t.setpos(-350, -100)
 t.pendown()
 
 
-# 便捷区函数
+"""便捷区函数"""
 def get_resource_path(relative_path):
     """打包exe时资源文件的路径"""
     if hasattr(sys, '_MEIPASS'):
@@ -28,22 +28,22 @@ def get_resource_path(relative_path):
 
 
 def whfd(x):
-    """无痕前进"""
+    # 无痕前进
     t.penup()
     t.forward(x)
     t.pendown()
 
 
 def whgt(x, y=None):
-    """无痕移位by位置"""
+    # 无痕移位by位置
     t.penup()
     t.goto(x, y)
     t.pendown()
 
 
-# 移动区函数
+"""移动区函数"""
 def change_button():
-    """点击切换坐标系"""
+    # 点击切换坐标系
     temp = button_northeast['text']
     button_northeast['text'] = button_southeast['text']
     button_southeast['text'] = button_southwest['text']
@@ -81,7 +81,7 @@ def southeast():
     t.fd(20)
 
 
-# 元件区函数
+"""元件区函数"""
 def gang(x=7):
     t.left(90)
     t.fd(x)
@@ -210,7 +210,7 @@ def quan():
     whgt(pos)
 
 
-# 功能区函数
+"""功能区函数"""
 def biaoti():
     word = theScreen.textinput("标题", "请输入标题内容")
     t.write(word, align='center', font=("宋体", 20, "bold"))
@@ -284,12 +284,16 @@ def manual():
     canva['cursor'] = 'crosshair'
 
 
-# 移动按键区
+"""移动按键区"""
+# 坐标图片
 photo = PhotoImage(file=get_resource_path("six.gif"))
 label_photo = Label(root, image=photo, borderwidth=0)
 label_photo.place(x=741, y=615, anchor='center')
+# 更改坐标
 button_change_button = Button(root, text="更改坐标方向", command=change_button).pack(side='right')
+# 手动施法
 button_manual = Button(root, text="手动施法", command=manual).place(x=0, y=670)
+# 方向按钮
 button_northeast = Button(root, text='北', command=northeast)
 button_southeast = Button(root, text='东', command=southeast)
 button_southwest = Button(root, text='南', command=southwest)
@@ -302,28 +306,41 @@ button_up = Button(root, text='上', command=up)
 button_up.place(x=726, y=520)
 button_down = Button(root, text='下', command=down)
 button_down.place(x=726, y=673)
-# 功能按键区
+"""功能按键区"""
 button_new_word = Button(root, text="添加文字", command=new_word).place(x=865, y=430)
 button_new = Button(root, text="新起点", command=new).place(x=865, y=465)
 button_biaoti = Button(root, text='点击添加标题（先用新起点设定位置）', command=biaoti).pack()
 button_long = Button(root, text="远距离", command=long).place(x=865, y=500)
-button_daduan = Button(root, text="打断", command=daduan).place(x=865, y=535)
 button_undo = Button(root, text='撤销', command=t.undo).place(x=865, y=570)
 button_clear = Button(root, text="清屏", command=t.clear).place(x=865, y=605)
 button_hide = Button(root, text='隐藏', command=t.hideturtle).place(x=865, y=640)
 button_show = Button(root, text='显示', command=t.showturtle).place(x=900, y=640)
 button_save = Button(root, text='保存', command=save).place(x=865, y=675)
 button_insert_image = Button(root, text="插图", command=insert_image).place(x=900, y=675)
-# 元件符号区
-button_shape_s = Button(root, text='起止', command=shape_s).place(x=100, y=640)
-button_falan = Button(root, text='法兰', command=falan).place(x=100, y=670)
-button_xiaofamen = Button(root, text='小阀', command=xiaofamen).place(x=150, y=670)
-button_dafamen = Button(root, text='大阀', command=dafamen).place(x=150, y=640)
-button_ltob = Button(root, text="小大", command=ltob).place(x=200, y=670)
-button_btol = Button(root, text="大小", command=btol).place(x=200, y=640)
-button_yibiao = Button(root, text="仪表", command=yibiao).place(x=250, y=670)
-button_gang = Button(root, text='画杠', command=gang).place(x=300, y=640)
-button_quan = Button(root, text='画圈', command=quan).place(x=300, y=670)
-# 逻辑结束
+"""元件符号区"""
+# 起止符号
+icon_shape_s = ImageTk.PhotoImage(Image.open(get_resource_path(r"icons\icon_shape_s.jpg")))
+button_shape_s = Button(root, image=icon_shape_s, command=shape_s).place(x=100, y=640)
+# 法兰
+icon_falan = ImageTk.PhotoImage(Image.open(get_resource_path(r"icons\icon_falan.jpg")))
+button_falan = Button(root, image=icon_falan, command=falan).place(x=100, y=670)
+# 异径管
+icon_btol = ImageTk.PhotoImage(Image.open(get_resource_path(r"icons\icon_btol.jpg")))
+button_btol = Button(root, image=icon_btol, command=btol).place(x=150, y=640)
+icon_ltob = ImageTk.PhotoImage(Image.open(get_resource_path(r"icons\icon_ltob.jpg")))
+button_ltob = Button(root, image=icon_ltob, command=ltob).place(x=150, y=670)
+# 测厚杠圈
+icon_gang = ImageTk.PhotoImage(Image.open(get_resource_path(r"icons\icon_gang.jpg")))
+button_gang = Button(root, image=icon_gang, command=gang).place(x=200, y=640)
+icon_quan = ImageTk.PhotoImage(Image.open(get_resource_path(r"icons\icon_quan.jpg")))
+button_quan = Button(root, image=icon_quan, command=quan).place(x=200, y=670)
+# 阀门
+button_xiaofamen = Button(root, text='小阀', command=xiaofamen).place(x=250, y=670)
+button_dafamen = Button(root, text='大阀', command=dafamen).place(x=250, y=640)
+# 仪表
+button_yibiao = Button(root, text="仪表", command=yibiao).place(x=300, y=640)
+# 打断
+button_daduan = Button(root, text="打断", command=daduan).place(x=300, y=670)
+"""逻辑结束"""
 theScreen.mainloop()
 root.mainloop()
