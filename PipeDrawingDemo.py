6a613f02264fc914836645ce8b5168ff3924a506
avebest1992 <77918703+avebest1992@users.noreply.github.com>
2021-03-01 19:55:31 +0800

    Update PipeDrawingDemo.py
    把一些功能按钮移到顶部菜单里面

--- PipeDrawingDemo.py
+++ PipeDrawingDemo.py
@@ -2,7 +2,7 @@ import turtle
 from tkinter import *
 import tkinter.filedialog
 from PIL import Image, ImageTk
-import os
+import os, time
 from tkinter import ttk
 
 """初始化屏幕、画布、画笔"""
@@ -212,9 +212,19 @@ def quan():
 
 
 """功能区函数"""
-def biaoti():
+def biaoti_action(event):
+    x, y = event.x - 476, -event.y + 351
+    whgt(x, y)
+    root.unbind("<Button-1>")
     word = theScreen.textinput("标题", "请输入标题内容")
     t.write(word, align='center', font=("宋体", 20, "bold"))
+    canva['cursor'] = 'arrow'
+
+
+def biaoti():
+    root.bind("<Button-1>", biaoti_action)
+    canva['cursor'] = 'crosshair'
+
 
 image_tb_inserted = None
 im = None
@@ -243,7 +253,7 @@ def long():
     t.fd(length)
 
 
-def start(event):
+def new_action(event):
     x, y = event.x - 476, -event.y + 351
     whgt(x, y)
     root.unbind("<Button-1>")
@@ -251,15 +261,16 @@ def start(event):
 
 
 def new():
-    root.bind("<Button-1>", start)
+    root.bind("<Button-1>", new_action)
     canva['cursor'] = 'crosshair'
 
 
 def save():
+    time.sleep(0.2)
     os.startfile(get_resource_path("snip.exe"))
 
 
-def word_action(event):
+def new_word_action(event):
     x, y = event.x - 476, -event.y + 351
     whgt(x, y)
     root.unbind("<Button-1>")
@@ -269,7 +280,7 @@ def word_action(event):
 
 
 def new_word():
-    root.bind("<Button-1>", word_action)
+    root.bind("<Button-1>", new_word_action)
     canva['cursor'] = 'crosshair'
 
 
@@ -288,18 +299,28 @@ def manual():
 """顶部菜单区"""
 menubar = Menu(root)
 root.config(menu=menubar)
-menu1 = Menu(menubar, tearoff=1)
-menu1.add_command(label="导入图片", command=insert_image)
-menu1.add_command(label="保 存", command=save)
-menu1.add_command(label="退 出", command=root.quit)
-menubar.add_cascade(label="文 件", menu=menu1)
+menu_files = Menu(menubar, tearoff=0)
+menu_files.add_command(label="导入图片", command=insert_image)
+menu_files.add_command(label="保 存", command=save)
+menu_files.add_command(label="退 出", command=root.quit)
+menubar.add_cascade(label="文 件", menu=menu_files)
+menu_edit = Menu(menubar, tearoff=1)
+menu_edit.add_command(label="清 屏", command=t.clear)
+menu_edit.add_command(label="更改坐标", command=change_button)
+menu_edit.add_separator()
+menu_edit.add_command(label="隐藏箭头", command=t.hideturtle)
+menu_edit.add_command(label="显示箭头", command=t.showturtle)
+menu_edit.add_separator()
+menu_edit.add_command(label="添加标题",command=biaoti)
+menu_edit.add_command(label="添加文字",command=new_word)
+menubar.add_cascade(label="编 辑", menu=menu_edit)
 """移动按键区"""
 # 坐标图片
 photo = PhotoImage(file=get_resource_path("six.gif"))
 label_photo = Label(root, image=photo, borderwidth=0)
 label_photo.place(x=741, y=615, anchor='center')
 # 更改坐标
-button_change_button = Button(root, text="更改坐标方向", command=change_button, relief='groove').pack(side='right')
+# button_change_button = Button(root, text="更改坐标方向", command=change_button, relief='groove').pack(side='right')
 # 手动施法
 button_manual = Button(root, text="手动施法", command=manual, relief='groove').place(x=0, y=670)
 # 方向按钮
@@ -316,16 +337,16 @@ button_up.place(x=726, y=520)
 button_down = Button(root, text='下', command=down, relief='groove')
 button_down.place(x=726, y=673)
 """功能按键区"""
-button_new_word = Button(root, text="添加文字", command=new_word, relief='groove').place(x=865, y=430)
-button_new = Button(root, text="新起点", command=new).place(x=865, y=465)
-button_biaoti = Button(root, text='点击添加标题（先用新起点设定位置）', command=biaoti, relief='groove').pack()
+# button_new_word = Button(root, text="添加文字", command=new_word, relief='groove').place(x=865, y=430)
+button_new = Button(root, text="新起点", command=new, relief='groove').place(x=865, y=465)
+# button_biaoti = Button(root, text='点击添加标题（先用新起点设定位置）', command=biaoti, relief='groove').pack()
 button_long = Button(root, text="远距离", command=long, relief='groove').place(x=865, y=500)
 button_undo = Button(root, text='撤销', command=t.undo, relief='groove').place(x=865, y=570)
-button_clear = Button(root, text="清屏", command=t.clear, relief='groove').place(x=865, y=605)
-button_hide = Button(root, text='隐藏', command=t.hideturtle, relief='groove').place(x=865, y=640)
-button_show = Button(root, text='显示', command=t.showturtle, relief='groove').place(x=900, y=640)
-button_save = Button(root, text='保存', command=save, relief='groove').place(x=865, y=675)
-button_insert_image = Button(root, text="插图", command=insert_image, relief='groove').place(x=900, y=675)
+# button_clear = Button(root, text="清屏", command=t.clear, relief='groove').place(x=865, y=605)
+# button_hide = Button(root, text='隐藏', command=t.hideturtle, relief='groove').place(x=865, y=640)
+# button_show = Button(root, text='显示', command=t.showturtle, relief='groove').place(x=900, y=640)
+# button_save = Button(root, text='保存', command=save, relief='groove').place(x=865, y=675)
+# button_insert_image = Button(root, text="插图", command=insert_image, relief='groove').place(x=900, y=675)
 """元件符号区"""
 # 起止符号
 icon_shape_s = ImageTk.PhotoImage(Image.open(get_resource_path(r"icons\icon_shape_s.jpg")))
