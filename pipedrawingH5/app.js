e21363f20bd20bf6fa8ca5e112b6a00b1c153c8c
avebest1992 <77918703+avebest1992@users.noreply.github.com>
2021-03-05 01:33:57 +0800

    更改定位标记的方法
    顺便更改button定义函数的表达

--- pipedrawingH5/app.js
+++ pipedrawingH5/app.js
@@ -1,51 +1,51 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const button_ne = document.getElementById("northeast");
-const button_se = document.getElementById("southeast");
-const button_sw = document.getElementById("southwest");
-const button_nw = document.getElementById("northwest");
-const button_up = document.getElementById("up");
-const button_down = document.getElementById("down");
+// const button_ne = document.getElementById("northeast");
+// const button_se = document.getElementById("southeast");
+// const button_sw = document.getElementById("southwest");
+// const button_nw = document.getElementById("northwest");
+// const button_up = document.getElementById("up");
+// const button_down = document.getElementById("down");
 let nowX = 100;
 let nowY = 500;
 let stamps = [];
 let stamp_count = 0;
 let stamp_id = 0;
-button_ne.onclick = function (){
+function button_ne(){
     ctx.moveTo(nowX, nowY);
     nowX += 17.32;
     nowY -= 10.00;
     ctx.lineTo(nowX, nowY);
     ctx.stroke();
 }
-button_se.onclick = function (){
+function button_se(){
     ctx.moveTo(nowX, nowY);
     nowX += 17.32;
     nowY += 10.00;
     ctx.lineTo(nowX, nowY);
     ctx.stroke();
 }
-button_sw.onclick = function (){
+function button_sw(){
     ctx.moveTo(nowX, nowY);
     nowX -= 17.32;
     nowY += 10.00;
     ctx.lineTo(nowX, nowY);
     ctx.stroke();
 }
-button_nw.onclick = function (){
+function button_nw(){
     ctx.moveTo(nowX, nowY);
     nowX -= 17.32;
     nowY -= 10.00;
     ctx.lineTo(nowX, nowY);
     ctx.stroke();
 }
-button_up.onclick = function (){
+function button_up(){
     ctx.moveTo(nowX, nowY);
     nowY -= 20.00;
     ctx.lineTo(nowX, nowY);
     ctx.stroke();
 }
-button_down.onclick = function (){
+function button_dn(){
     ctx.moveTo(nowX, nowY);
     nowY += 20.00;
     ctx.lineTo(nowX, nowY);
@@ -54,23 +54,29 @@ button_down.onclick = function (){
 function add_stamp() {
     stamps[stamp_count] = [nowX, nowY];
     stamp_count += 1;
+    document.getElementById("stamp_count").innerText = String(stamp_count);
+    document.getElementById("stamp_id").max = stamp_count;
 }
 function goto_stamp() {
-    if (stamps.length === 0){ return;}
-    if (stamp_id < stamp_count)
-    {
-        nowX = stamps[stamp_id][0];
-        nowY = stamps[stamp_id][1];
-        stamp_id += 1;
-    }
-    else {
-        nowX = stamps[0][0];
-        nowY = stamps[0][1];
-        stamp_id = 1;
-    }
+    let stamp_index = document.getElementById("stamp_id").value - 1;
+    nowX = stamps[stamp_index][0];
+    nowY = stamps[stamp_index][1];
+    // if (stamps.length === 0){ return;}
+    // if (stamp_id < stamp_count)
+    // {
+    //     nowX = stamps[stamp_id][0];
+    //     nowY = stamps[stamp_id][1];
+    //     stamp_id += 1;
+    // }
+    // else {
+    //     nowX = stamps[0][0];
+    //     nowY = stamps[0][1];
+    //     stamp_id = 1;
+    // }
 }
 function clear_stamp() {
     stamps = [];
     stamp_count = 0;
     stamp_id = 0;
+    document.getElementById("stamp_count").innerText = String(stamp_count);
 }
