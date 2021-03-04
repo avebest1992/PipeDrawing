62df77bf6b7026304fa66b2f438ca60f00a67cee
avebest1992 <77918703+avebest1992@users.noreply.github.com>
2021-03-05 04:42:20 +0800

    增加滑块定位指针功能
    （玛德好坑，·<input type="range">·输入的value竟然是***string！！！***，害得我搞了大半夜不知道哪里出bug！！！！）

--- pipedrawingH5/app.js
+++ pipedrawingH5/app.js
@@ -1,5 +1,11 @@
+let screenW = document.documentElement.clientWidth;
+let screenH = document.documentElement.clientHeight;
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+canvas.width = screenW - 50;
+canvas.height = screenH -200;
+document.getElementById("x_slider").max = String(canvas.width);
+document.getElementById("y_slider").max = String(canvas.height);
 // const button_ne = document.getElementById("northeast");
 // const button_se = document.getElementById("southeast");
 // const button_sw = document.getElementById("southwest");
@@ -7,7 +13,7 @@ const ctx = canvas.getContext("2d");
 // const button_up = document.getElementById("up");
 // const button_down = document.getElementById("down");
 let nowX = 100;
-let nowY = 500;
+let nowY = 300;
 let stamps = [];
 let stamp_count = 0;
 let stamp_id = 0;
@@ -79,4 +85,17 @@ function clear_stamp() {
     stamp_count = 0;
     stamp_id = 0;
     document.getElementById("stamp_count").innerText = String(stamp_count);
+}
+function show_XY() {
+    // nowX = Number(document.getElementById("x_slider").value);
+    // nowY = Number(document.getElementById("y_slider").value);
+    // document.getElementById("show_XY").innerText = "( "+String(nowX)+" , "+String(nowY)+" )";
+    let tempX, tempY;
+    tempX = document.getElementById("x_slider").value;
+    tempY = document.getElementById("y_slider").value;
+    document.getElementById("show_XY").innerText = "( "+tempX+" , "+tempY+" )";
+}
+function set_XY(){
+    nowX = Number(document.getElementById("x_slider").value);
+    nowY = Number(document.getElementById("y_slider").value);
 }
